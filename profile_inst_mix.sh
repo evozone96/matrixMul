30eadc9b0b9a2a028cf7b922f81ea5397b3ade00
Michael Yang <michael.y.8d@gmail.com>
2019-04-09 22:51:07 -0400

    push scripts to repo

---
+++ profile_inst_mix.sh
@@ -0,0 +1,4 @@
+!# /usr/bin/env bash
+
+a=`cat $1`
+nv-nsight-cu-cli -f --metrics sm__inst_executed_pipe_adu.avg,sm__inst_executed_pipe_alu.avg,sm__inst_executed_pipe_cbu.avg,sm__inst_executed_pipe_fma.avg,sm__inst_executed_pipe_fp16.avg,sm__inst_executed_pipe_fp64.avg,sm__inst_executed_pipe_ipa.avg,sm__inst_executed_pipe_lsu.avg,sm__inst_executed_pipe_tensor.avg,sm__inst_executed_pipe_tex.avg,sm__inst_executed_pipe_uniform.avg,sm__inst_executed_pipe_xu.avg,gpu__cycles_elapsed.avg  $a
